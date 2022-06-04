8ca4c985ef102da6d9895738c81b559594058af5
JulianColihuinca <freestyle319@gmail.com>
2022-06-04 00:05:14 -0300

    MVCControlador
    Agregado ControladorMonitor

---
+++ src/MVCMonitor/ControladorMonitor.java
@@ -0,0 +1,18 @@
+package MVCMonitor;
+
+import java.util.Observable;
+import java.util.Observer;
+
+public class ControladorMonitor implements Observer {
+	
+	private VistaMonitor vista;
+
+	@Override
+	public void update(Observable o, Object arg) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+	
+
+}
